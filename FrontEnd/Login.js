931e1feace950280db5a1d19d4492b39ce6380b0
Nekhal <119949021+Nekhal@users.noreply.github.com>
2023-01-27 12:55:39 +0100

    Add files via upload

--- FrontEnd/Login.js
+++ FrontEnd/Login.js
@@ -3,18 +3,17 @@
 /*On déclare nos variables*/
 let inputEmail = document.querySelector('#signin-email');
 let inputPassword = document.querySelector('#signin-password');
-let errorMessage = document.querySelector('#errorMsg');
-//
-const submitBtn = document.querySelector('input[type="submit"]');
+let msgLogDiv = document.querySelector('#errorMsg');
+
+const submitBtn = document.querySelector('#signin-btn-submit');
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    errorMessage.innerHTML = "";
-    let userEmail = document.querySelector('#signin-email').value;
-    let userPassword = document.querySelector('#signin-password').value;
-    console.log(userEmail+userPassword);
+    msgLogDiv.innerHTML = "";
+    const userEmail = document.querySelector('#signin-email').value;
+    const userPassword = document.querySelector('#signin-password').value;    
     logIn(userEmail,userPassword);
 });
-//
+
 function logIn(email,password) {
     fetch("http://localhost:5678/api/users/login", {
     method: 'POST',
@@ -26,7 +25,8 @@ function logIn(email,password) {
         {
             email: email,
             password: password
-        })
+        }
+        )
     })
     .then(function(res) {
         return res.json();
@@ -35,26 +35,33 @@ function logIn(email,password) {
         let msgLog = "";
         let userId = value.id;
         let userToken = value.token;
-        /*C'est là qu'on compare les 2 strings (mail+mdp) à l'ID enregistré+ son token*/
-        userId !== null && userToken != null ? storeTkn(userId, userToken) : msgLog = "Erreur dans l’identifiant ou le mot de passe";        
-        errorMessage.innerHTML += msgLog;        
+        // On vérifie que la combinaison email+password correspond bien à un id et un token
+        userId !== null && userToken != null ? storeTkn(userId, userToken) : msgLog = "Erreur dans l’identifiant ou le mot de passe";
+        // Sinon on affiche un message d'erreur
+        msgLogDiv.innerHTML += msgLog;
+        // vérification
     })
-    .catch(function(err) {        
+    .catch(function(err) {
+        // message d'erreur
         console.log(err);
-    })    
+    })
 };
-/*Storage du token*/
+
+// mise en storage du token
 function storeTkn(id, token) {
     sessionStorage.setItem("id", id);
-    sessionStorage.setItem("token", token);    
+    sessionStorage.setItem("token", token);
+    // alert(id+"/"+token);
     window.location.href = "index.html";
 };
-/*RAZ du message d'erreur*/
+
+// Au click dans un des deux input de login on clean la div de message d'erreur de connexion
 inputEmail.addEventListener('click', () => {
-    errorMessage.textContent = '';
+    msgLogDiv.textContent = '';
 });
+
 inputPassword.addEventListener('click', () => {
-    errorMessage.textContent = '';
+    msgLogDiv.textContent = '';
 });
 
 /*FIN DU LOGIN*/
@@ -70,6 +77,6 @@ loginToProjet[0].addEventListener('click', linkProjets);
 /*Retour vers le formulaire de contact depuis la page LogIn*/
 const loginToContact = document.querySelectorAll("#retContact");
 function linkContact() {
-    window.location.href = "index.html#contact";
+    window.location.href = "index.html#footer";
 };
 loginToContact[0].addEventListener('click', linkContact);
--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,6 +1,8 @@
 const reponse = await fetch('http://localhost:5678/api/works');
 const images = await reponse.json();
 
+
+/*Création de la galerie */
 function genererImages(images){
    for (let i = 0; i < images.length; i++) {
 
@@ -27,7 +29,7 @@ function genererImages(images){
 }
 
 /*Permet l'affichage de la gallerie*/ 
-//genererImages(images);
+genererImages(images);
 
 
 /*---------LES FILTRES---------*/
@@ -74,3 +76,97 @@ boutonHotels.addEventListener("click", function () {
     genererImages(imagesHotel)    
 });
 
+
+/*MODALE 1*/
+
+
+/*Génerer la galerie de la modale*/
+function genererMiniImages(images){
+    for (let i = 0; i < images.length; i++) {
+ 
+        const miniArticle = images[i];
+        /*Récupération de l'élément du DOM qui accueillera les images*/
+        const miniGalery = document.querySelector(".miniGalery");
+        /*Création d’une balise dédiée à une image*/
+        const minBlocElement = document.createElement("figure");
+        /*Création des balises*/ 
+        const minImageElement = document.createElement("img");
+        minImageElement.src = miniArticle.imageUrl;
+        const minNomElement = document.createElement("figcaption");
+        minNomElement.innerText = "éditer";
+        /*Balise utilisée pour les filtres */ 
+        const minCategoryElement = document.createElement("p");
+        minCategoryElement.innerText = miniArticle.categoryId;
+        /*Création du bouton "déplacer"*/
+        const moveIcon = document.createElement("span");        
+        const moveIconContent = document.createElement("i");
+        moveIconContent.setAttribute('class', 'fa-solid fa-arrows-up-down-left-right');
+        /*Création du bouton "Supprimer"*/
+        const supprimIcon = document.createElement("span");
+        const supprimIconContent = document.createElement("i");
+        supprimIconContent.setAttribute('class','fa-solid fa-trash-can' )
+
+     
+        /*On rattache la balise figure à la section gallery*/
+        miniGalery.appendChild(minBlocElement);        
+        /*On rattache l’image et sa description à la balise figure*/
+        minBlocElement.appendChild(minImageElement);
+        minBlocElement.appendChild(minNomElement);        
+        /*On rattache l'icone de déplacement et on lui attribut une classe*/ 
+        minBlocElement.appendChild(moveIcon);
+        moveIcon.appendChild(moveIconContent);
+        moveIcon.setAttribute('class', 'moveIcon');        
+        /*Rattachement de l'icone "supprimer" et on lui attribut une classe*/
+        minBlocElement.appendChild(supprimIcon);
+        supprimIcon.appendChild(supprimIconContent);
+        supprimIcon.setAttribute('class','trashIcon');
+
+        /*Ajout d'un évènement "mouseover" pour afficher l'icone de déplacement*/
+        minBlocElement.addEventListener("mouseover", function () {
+        moveIcon.style.display = "block";
+        });
+        /*Ajout d'un évènement "mouseout" pour cacher l'icone de déplacement*/
+        minBlocElement.addEventListener("mouseout", function () {
+        moveIcon.style.display = "none";
+        });
+     };     
+ } 
+
+ /*Permet l'affichage de la galerie de la modale*/ 
+ genererMiniImages(images);
+
+
+// Vérification du token de la session
+let userSessionId = sessionStorage.getItem("id");
+let userSessionToken = sessionStorage.getItem("token");
+
+// Ouverture du mode édition si la session est bien composée d'un Id et d'un Token
+userSessionId && userSessionToken ? editorMode() : "";
+
+function editorMode() {
+    //Apparition des div de modification ainsi que du bandeau du haut
+    document.querySelectorAll(".connected").display = "block";
+    //Disparition des filtres
+    document.querySelector(".filtres").display = "none";
+    //Changement du logIn et logOut
+    styleLog = document.querySelectorAll("#loginBtn");
+    styleLog[2].textContent = "logout";
+}
+
+
+// Fonction pour rediriger au clic sur login
+function linkLogin() {
+    window.location.href = "login.html";
+};
+
+// Fonction pour supprimer la session au clic sur logout
+function linkLogout() {
+    sessionStorage.clear();
+    window.location.href = "index.html";
+};
+
+// Intéraction avec le menu via login
+styleLog = document.querySelector("#loginBtn");
+
+// en fonction de l'état connecté ou non connecté
+userSessionId && userSessionToken ? styleLog[2].addEventListener("click", linkLogout) : styleLog[2].addEventListener("click", linkLogin);
